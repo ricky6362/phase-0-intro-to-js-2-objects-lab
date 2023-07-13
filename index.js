c838b08fc8ec6937abb98665a9ce92b597513539
ricky6362 <63843083+ricky6362@users.noreply.github.com>
2023-07-13 11:02:20 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution in this file!
+const employee = {
+    name: "Joel Otuko",
+    streetAddress: "415 street" 
+} 
+
+// updating the items in an array non-destructively using a function
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const myNewObject = {
+        ...employee,
+        [key]: value
+    }
+
+    return myNewObject;
+}
+
+// destructively updating an arrays content
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee
+}
+
+// Non-destructive
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee}
+    delete newEmployee[key]
+    return newEmployee
+}
+
+// Destructive 
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
+
+
+
